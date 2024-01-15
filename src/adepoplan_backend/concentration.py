333c9e671a0063c5a33c3ef21e57e284431500b4
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2024-01-15 12:47:06 +0100

    Add pipeline for creating concentration files

--- src/adepoplan_backend/concentration.py
+++ src/adepoplan_backend/concentration.py
@@ -1,42 +1,42 @@
 def make_concentration(config):
+    import ladim_aggregate
+
+    ladim_output_file = config['root_dir'] / config['particles']['file']
+
     crecon_count_fname = config['root_dir'] / config['concentration']['count_conf_file']
-    with open(crecon_count_fname, 'w', encoding='utf-8') as fp:
-        create_crecon_file_for_particle_counting(fp)
-
     count_fname = config['root_dir'] / config['concentration']['count_file']
-    create_particle_count_file(crecon_count_fname, count_fname)
+    with open(crecon_count_fname, 'w', encoding='utf-8') as fp:
+        create_crecon_file_for_particle_counting(fp, count_fname, ladim_output_file)
 
-    crecon_conc_file = config['root_dir'] / config['concentration']['conc_conf_file']
-    with open(crecon_conc_file, 'w', encoding='utf-8') as fp:
-        create_crecon_file_for_concentration(fp)
+    ladim_aggregate.main(str(crecon_count_fname))
 
+    crecon_conc_fname = config['root_dir'] / config['concentration']['conc_conf_file']
     conc_fname = config['root_dir'] / config['concentration']['conc_file']
-    create_concentration_file(crecon_conc_file, conc_fname)
+    with open(crecon_conc_fname, 'w', encoding='utf-8') as fp:
+        create_crecon_file_for_concentration(fp, conc_fname, ladim_output_file)
+
+    ladim_aggregate.main(str(crecon_conc_fname))
 
 
-def create_crecon_file_for_particle_counting(stream):
+def create_crecon_file_for_particle_counting(stream, outfile, ladim_file):
     import importlib.resources
     from . import templates
     resource_file = importlib.resources.files(templates).joinpath('crecon1.yaml')
-    txt = resource_file.read_text(encoding='utf-8')
+    txt_template = resource_file.read_text(encoding='utf-8')
+    txt = txt_template.format(
+        crecon_output_file=outfile,
+        ladim_output_file=ladim_file,
+    )
     stream.write(txt)
 
 
-def create_crecon_file_for_concentration(stream):
+def create_crecon_file_for_concentration(stream, outfile, ladim_file):
     import importlib.resources
     from . import templates
     resource_file = importlib.resources.files(templates).joinpath('crecon1.yaml')
-    txt = resource_file.read_text(encoding='utf-8')
+    txt_template = resource_file.read_text(encoding='utf-8')
+    txt = txt_template.format(
+        crecon_output_file=outfile,
+        ladim_output_file=ladim_file,
+    )
     stream.write(txt)
-
-
-def create_particle_count_file(input_file, output_file):
-    import xarray as xr
-    conc_dset = xr.Dataset()
-    conc_dset.to_netcdf(output_file)
-
-
-def create_concentration_file(input_file, output_file):
-    import xarray as xr
-    conc_dset = xr.Dataset()
-    conc_dset.to_netcdf(output_file)
--- tests/test_concentration.py
+++ tests/test_concentration.py
@@ -6,7 +6,11 @@ import yaml
 class Test_create_crecon_file_for_particle_counting:
     def test_creates_valid_yaml_file(self):
         buf = io.StringIO()
-        concentration.create_crecon_file_for_particle_counting(buf)
+        concentration.create_crecon_file_for_particle_counting(
+            stream=buf,
+            outfile='my_outfile.yaml',
+            ladim_file='ladim_output.nc',
+        )
         buf.seek(0)
         result = yaml.safe_load(buf)
         assert isinstance(result, dict)
@@ -15,7 +19,11 @@ class Test_create_crecon_file_for_particle_counting:
 class Test_create_crecon_file_for_concentration:
     def test_creates_valid_yaml_file(self):
         buf = io.StringIO()
-        concentration.create_crecon_file_for_concentration(buf)
+        concentration.create_crecon_file_for_concentration(
+            stream=buf,
+            outfile='my_outfile.yaml',
+            ladim_file='ladim_output.nc',
+        )
         buf.seek(0)
         result = yaml.safe_load(buf)
         assert isinstance(result, dict)
