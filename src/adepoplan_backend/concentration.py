7a869c9f89666191267913e456a5685c1786b76f
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2024-01-18 15:29:27 +0100

    Remove obsolete functions

--- src/adepoplan_backend/concentration.py
+++ src/adepoplan_backend/concentration.py
@@ -28,7 +28,7 @@ def make_concentration(config):
     feed_factor = config['user_input']['feed_factor']
     weight_file = config['root_dir'] / config['concentration']['weight_file']
     with open(crecon_conc_fname, 'w', encoding='utf-8') as fp:
-        create_crecon_file_for_concentration(
+        create_config_file(
             stream=fp,
             outfile=conc_fname,
             ladim_file=ladim_output_file,
@@ -44,32 +44,32 @@ def make_concentration(config):
     ladim_aggregate.main(str(crecon_conc_fname))
 
 
-def create_count(outfile, ladim_file):
-    import ladim_aggregate
-    conf = dict(
-        bins=dict(poly_id='group_by', release_time=86400),
-        filter_particle="pid > -1",
-        infile=ladim_file,
-        outfile=outfile,
-    )
-    ladim_aggregate.run_conf(conf)
-
-
 def create_weights(feed_df, ladim_dset):
     import numpy as np
-    import xarray as xr
-    import netCDF4 as nc
 
     # Convert from long form to table form
-    feed_df['release_time'] = feed_df['time'].values.astype('datetime64[s]').astype('int64')
-    feed_df = feed_df[['release_time', 'poly_id', 'feed']].set_index(['release_time', 'poly_id'])
-    feed_dset = feed_df.to_xarray()
-    feed_dset['feed'] = feed_dset['feed'].fillna(0)
+    feed_dset = convert_feed_table_to_matrix_form(feed_df)
 
-    # Count number of particles released for each category
+    # Count number of particles, for each entry in feed matrix
+    feed_dset['pcount'] = count_particles_per_cage_and_dates(
+        ladim_dset=ladim_dset,
+        poly_id=feed_dset['poly_id'].values,
+        dates=feed_dset['release_time'].values,
+    )
+
+    # Compute feed per particle
+    feed_dset['feed_per_particle'] = feed_dset['feed'] / np.maximum(feed_dset['pcount'], 1)
+    return feed_dset
+
+
+def count_particles_per_cage_and_dates(ladim_dset, poly_id, dates):
     import ladim_aggregate
+    import netCDF4 as nc
+    import numpy as np
+    import xarray as xr
+
     with nc.Dataset('count.nc', 'w', diskless=True) as count_dset:
-        time_bins = feed_dset['release_time'].values.tolist()
+        time_bins = list(dates)
         last_entry = np.diff(time_bins[-2:]) + time_bins[-1]
         time_bins += last_entry.tolist()
         conf = dict(
@@ -79,14 +79,12 @@ def create_weights(feed_df, ladim_dset):
             outfile=count_dset,
         )
         ladim_aggregate.run_conf(conf)
-        feed_dset['pcount'] = xr.DataArray(
+        return xr.DataArray(
             data=count_dset.variables['histogram'][:],
             coords=dict(poly_id=count_dset.variables['poly_id'][:]),
             dims=('release_time', 'poly_id'),
-        ).sel(poly_id=feed_dset['poly_id'])
+        ).sel(poly_id=poly_id)
 
-    feed_dset['feed_per_particle'] = feed_dset['feed'] / np.maximum(feed_dset['pcount'], 1)
-    return feed_dset
 
 def convert_feed_table_to_matrix_form(feed_df):
     feed_df['release_time'] = feed_df['time'].values.astype('datetime64[s]').astype('int64')
@@ -96,19 +94,7 @@ def convert_feed_table_to_matrix_form(feed_df):
     return feed_dset
 
 
-def create_crecon_file_for_particle_counting(stream, outfile, ladim_file):
-    import importlib.resources
-    from . import templates
-    resource_file = importlib.resources.files(templates).joinpath('crecon1.yaml')
-    txt_template = resource_file.read_text(encoding='utf-8')
-    txt = txt_template.format(
-        crecon_output_file=outfile,
-        ladim_output_file=ladim_file,
-    )
-    stream.write(txt)
-
-
-def create_crecon_file_for_concentration(
+def create_config_file(
         stream, outfile, ladim_file, feed_factor, weight_file,
         cell_area, max_age, latdiff, londiff,
 ):
@@ -127,24 +113,3 @@ def create_crecon_file_for_concentration(
         londiff=londiff,
     )
     stream.write(txt)
-
-
-def compute_feed_rate(feed_df):
-    import numpy as np
-    import xarray as xr
-
-    # Convert from long form to table form
-    feed_df['release_time'] = feed_df['time'].values.astype('datetime64[s]').astype('int64')
-    feed_df = feed_df[['release_time', 'poly_id', 'feed']].set_index(['release_time', 'poly_id'])
-    feed_dset = feed_df.to_xarray()
-    feed_dset['feed'] = feed_dset['feed'].fillna(0)
-
-    # Compute feed rate (kg per day)
-    sec_diff = np.diff(feed_dset['release_time'].values.astype('datetime64[s]').astype('int64'))
-    sec_diff = xr.Variable('release_time', np.concatenate([sec_diff, [sec_diff[-1]]]))
-    days_diff = sec_diff / (60 * 60 * 24)
-    feed_dset['rate'] = feed_dset['feed'] / days_diff
-    feed_dset['rate'].attrs['units'] = 'kg/day'
-    feed_dset['rate'].attrs['long_name'] = 'mean feeding rate'
-
-    return feed_dset['rate']
--- tests/test_concentration.py
+++ tests/test_concentration.py
@@ -5,15 +5,13 @@ import io
 import yaml
 import pandas as pd
 import xarray as xr
-import netCDF4 as nc
-from uuid import uuid4
 import numpy as np
 
 
-class Test_create_crecon_file_for_concentration:
+class Test_create_config_file:
     def test_creates_valid_yaml_file(self):
         buf = io.StringIO()
-        concentration.create_crecon_file_for_concentration(
+        concentration.create_config_file(
             stream=buf, outfile="conc_fname", ladim_file="ladim_output_file",
             feed_factor=1.2, weight_file="weight_file",
             cell_area=100, max_age=100,
@@ -22,48 +20,6 @@ class Test_create_crecon_file_for_concentration:
         buf.seek(0)
         result = yaml.safe_load(buf)
         assert isinstance(result, dict)
-
-
-class Test_compute_feed_rate:
-    def test_returns_kg_per_day_and_cage(self):
-        feed_df = pd.DataFrame(dict(
-            time=['1970-01', '1970-02', '1970-03', '1970-01', '1970-02'],
-            cage_id=['A', 'A', 'A', 'B', 'B'],
-            feed=[31, 28, 56, 310, 280],
-            poly_id=[1001, 1001, 1001, 1002, 1002],
-        ))
-        result = concentration.compute_feed_rate(feed_df)
-        assert result.name == 'rate'
-        assert result.dims == ('release_time', 'poly_id')
-        assert result['release_time'].values.tolist() == [0, 2678400, 5097600]
-        assert result['poly_id'].values.tolist() == [1001, 1002]
-        assert result.values.tolist() == [[1, 10], [1, 10], [2, 0]]
-
-
-class Test_create_count:
-    @pytest.fixture()
-    def outfile(self):
-        with nc.Dataset(uuid4(), 'w', diskless=True) as dset:
-            yield dset
-
-    @pytest.fixture()
-    def ladim_file(self):
-        return xr.Dataset(
-            coords=dict(time=[100, 20000, 100000]),
-            data_vars=dict(
-                release_time=xr.Variable('particle', [100, 20000, 100000]),
-                poly_id=xr.Variable('particle', [1001, 1004, 1005]),
-                particle_count=xr.Variable('time', [1, 2, 3]),
-                pid=xr.Variable('particle_instance', [0, 0, 1, 0, 1, 2]),
-            ),
-        )
-
-    def test_counts_particles_by_release_time_and_cage(self, outfile, ladim_file):
-        concentration.create_count(outfile, ladim_file)
-
-        assert outfile.variables['release_time'][:].tolist() == [43200, 129600]
-        assert outfile.variables['poly_id'][:].tolist() == [1001, 1004, 1005]
-        assert outfile.variables['histogram'][:].tolist() == [[1, 0], [1, 0], [0, 1]]
 
 
 class Test_create_weights:
